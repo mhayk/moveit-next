c14b79318ae8642166e93884314a942575dbd928
Mhayk Whandson <hi@mhayk.com>
2021-02-23 19:29:54 +0000

    Profile component has been imported.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
-import Head from 'next/head'
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from '../components/Profile';
+
+import style from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={style.container}>
       <ExperienceBar />
+
+      <section>
+        <div className="">
+          <Profile />
+        </div>
+        <div className=""></div>
+      </section>
     </div>
   )
 }
