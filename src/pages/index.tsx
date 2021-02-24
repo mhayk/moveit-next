f1d75983248e264d93e64ba5b4676bc2f0ca50d5
Mhayk Whandson <hi@mhayk.com>
2021-02-24 14:39:47 +0000

    ChallengeBox component has been imported in the Home page.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import Head from 'next/head';
+
 import { CompleteChallenges } from "../components/CompleteChallenges";
 import { CountDown } from "../components/Countdown";
+import { ChallengeBox } from "../components/ChallengeBox";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 
-import Head from 'next/head';
 
 import style from '../styles/pages/Home.module.css';
 
@@ -22,7 +24,7 @@ export default function Home() {
           <CountDown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
