41c8ef32ed6f2599280cec639778f405bd5f4509
Mhayk Whandson <hi@mhayk.com>
2021-02-24 18:31:32 +0000

    Some new skills have been added in the context.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,11 +1,21 @@
-import { createContext, ReactNode, useState } from 'react'
+import { createContext, ReactNode, useState } from 'react';
+import challengens from '../../challenges.json';
+
+interface Challenge {
+    type: 'body' | 'eye';
+    description: string;
+    amount: number;
+}
 
 interface ChallengesContextData {
     level: number;
     currentExperience: number;
     challengesCompleted: number;
+    activeChallenge: Challenge;
+    experienceToNextLevel: number;
     levelUp: () => void;
     startNewChallenge: () => void;
+    resetChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -18,13 +28,23 @@ export function ChallangesProvider({ children }: ChallengesProviderProps) {
     const [level, setLevel] = useState(1);
     const [currentExperience, setCurrentExperience] = useState(0);
     const [challengesCompleted, setChallengesCompleted] = useState();
+    const [activeChallenge, setActiveChallenge] = useState(null);
+
+    const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
     function levelUp() {
         setLevel(level + 1);
     }
 
     function startNewChallenge() {
-        console.log("New challenge")
+        const randomChallengeIndex = Math.floor(Math.random() * challengens.length)
+        const challenge = challengens[randomChallengeIndex];
+
+        setActiveChallenge(challenge);
+    }
+
+    function resetChallenge() {
+        setActiveChallenge(null);
     }
 
     return (
@@ -34,7 +54,10 @@ export function ChallangesProvider({ children }: ChallengesProviderProps) {
                 currentExperience,
                 challengesCompleted,
                 levelUp,
-                startNewChallenge
+                startNewChallenge,
+                activeChallenge,
+                resetChallenge,
+                experienceToNextLevel
             }}>
             {children}
         </ChallengeContext.Provider>
