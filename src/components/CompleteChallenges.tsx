ce316196f7c72e2895a1ef1b26ae7d516194487c
Mhayk Whandson <hi@mhayk.com>
2021-02-24 18:32:27 +0000

    Context has been imported in CompleteChallenges component.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/CompleteChallenges.tsx
+++ src/components/CompleteChallenges.tsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
+import { ChallengeContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/CompleteChallenges.module.css';
 
 export function CompleteChallenges() {
+    const { challengesCompleted } = useContext(ChallengeContext);
+
     return (
         <div className={styles.completedChallangesContainer}>
             <span>Challenges Complete</span>
-            <span>5</span>
+            <span>{challengesCompleted}</span>
         </div>
     )
 }
