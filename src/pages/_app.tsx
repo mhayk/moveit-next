488759229053c01578621470818d642bf3449e20
Mhayk Whandson <hi@mhayk.com>
2021-02-24 17:46:41 +0000

    The challenge context has been registered to be used.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
+import { ChallangesProvider } from "../contexts/ChallengesContext"
+
 import "../styles/global.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChallangesProvider>
+      <Component {...pageProps} />
+    </ChallangesProvider>
+  )
 }
 
 export default MyApp
